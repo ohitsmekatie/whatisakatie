74324b1eabb80b2bd7bd209c8f33fbf984e08d20
katie <katie.sipos@gmail.com>
2025-04-07 13:41:12 -0400

    project scaffold

---
+++ app/routes.py
@@ -0,0 +1,18 @@
+from app import app
+from flask import render_template
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/resume")
+def resume():
+    return render_template("resume.html")
